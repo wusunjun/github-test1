a2c29258018f381fac1194fb449bc1baaffd147c
raymond wu <raymond.wu@github.com>
2017-08-28 12:08:42 +1000

    first commit
    VS console app

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public string GetAgeEvaluation(int age)
+        {
+            if (age >= 0 && age <= 20)
+            { return "baby"; }
+            else if (age > 20 && age <= 100)
+            { return "adult"; }
+            else if (age < 0)
+            { throw new Exception("Are you human?"); } //抛出程序自定义的异常
+            else
+            { throw new Exception("Do u live too long?"); }
+        }
+    }
+}
